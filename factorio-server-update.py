f040241f441b0c2dd5875bb872ef2205530cbadd
Aidan <130617042+yumyumgummy11@users.noreply.github.com>
2025-01-08 22:54:27 -0700

    server-updater-v1

---
+++ factorio-server-update.py
@@ -0,0 +1,86 @@
+from requests import get
+from shutil import copyfile, copytree
+import datetime
+import os
+import tarfile
+
+def get_latest_server(fp):
+    url = "https://factorio.com/get-download/stable/headless/linux64"
+    filename = "/factorio-headless-stable.tar.xz"
+
+    print("Downloading latest stable server...")
+    temp = get(url, allow_redirects=True)
+
+    print("Download Complete! Saving file to:", fp + filename)
+
+    open(fp + filename, 'wb').write(temp.content)
+
+def get_time():
+    time = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    return(time)
+
+def get_server_dir():
+    try:
+        fp = input("Enter fp to location server is stored in:")
+        if 'factorio' in os.listdir(fp + '/factorio/bin/x64'):
+            return(fp)
+        else:
+            print("Incorrect filepath")
+            quit()
+    except Exception as error:
+        print(error)
+        quit()
+
+def rename_server(fp, time):
+    print("Renaming existing server...")
+    try:
+        os.rename(fp + '/factorio' , fp + '/factorio' + time)
+    except Exception as error:
+        print(error)
+        quit()
+
+def extract_tar(fp, time):
+    rename_server(fp, time)
+
+    print("Extracting tar...")
+    try:
+        with tarfile.open(fp + '/factorio-headless-stable.tar.xz', 'r') as tar:
+            tar.extractall(fp)
+    except Exception as error:
+        print(error)
+
+def copy_server_data(fp,time,copy_mods):
+    files_to_copy = ['/achievements.dat', '/player-data.json', '/data/server-settings.json', '/data/server-whitelist.json']
+    print("Copying server files...")
+    for i in files_to_copy:
+        try:
+            copyfile(fp + '/factorio' + time + i, fp + '/factorio' + i)
+            print("Copied", i)
+        except FileNotFoundError as error:
+            print(error)
+            print("one or more of the following files failed to copy", files_to_copy)
+            print("check that these files exist, you may have to copy them manually")
+    if copy_mods:
+        print("Copying mods...")
+        try:
+            copytree(fp + '/factorio' + time + '/mods', fp + '/factorio/mods')
+        except Exception as error:
+            print(error)
+            print("Unable to copy mods")
+
+
+fp = get_server_dir()
+time = get_time()
+
+if input("would you like to copy mods? y/n:").lower() == 'y':
+    copy_mods = True
+else:
+    copy_mods = False
+
+get_latest_server(fp)
+
+extract_tar(fp, time)
+
+copy_server_data(fp,time,copy_mods)
+
+print("Done")
